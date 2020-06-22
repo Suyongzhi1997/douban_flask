5e460afb1985ce012576b36142882a7748457ded
Super <1125699801@qq.com>
2020-06-22 10:17:49 +0800

    重构让子页面继承模板

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/index')
+def home():
+    return index()
+
+
+@app.route('/movie')
+def movie():
+    data_list = list()
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select * from movie250"
+    data = cur.execute(sql)
+    for item in data:
+        data_list.append(item)
+    cur.close()
+    con.close()
+    print(data_list)
+    return render_template("movie.html", movies=data_list)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
